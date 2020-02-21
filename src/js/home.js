420186f19e74ea3316433a4bff22340d18ba5186
Karla Agraz <karla.agraz@gmail.com>
2020-02-21 09:38:44 -0500

    manejo de errores

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,13 @@
 (async function load() {
-  // await
-  // action
-  // terror
-  // animation
   async function getData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+    if (data.data.movie_count > 0){
+      // aqui se acaba
+      return data;
+    }
+  // si no hay pelis aqui continua
+  throw new Error('No se encontró ningun resultado');
   }
 
   const $form = document.getElementById("form");
@@ -46,16 +47,23 @@
     });
     $featuringContainer.append($loader);
 
-    const data = new FormData($form);
-    const {
-      data: { movies: pelis }
-    } = await getData(
-      `${BASE_API}list_movies.json?limit=1&query_term=${data.get("name")}`
-    );
+      const data = new FormData($form);
+    try {
+      const {
+        data: {
+          movies: pelis
+        }
+      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
 
-    const HTMLString = featuringTemplate(pelis[0]);
-    $featuringContainer.innerHTML = HTMLString;
-  });
+      const HTMLString = featuringTemplate(pelis[0]);
+      $featuringContainer.innerHTML = HTMLString;
+    } catch(error) {
+      alert(error.message);
+      $loader.remove();
+      $home.classList.remove('search-active')
+    }
+  })
+  
 
 function videoItemTemplate(movie, category) {
     return `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category=${category}>
